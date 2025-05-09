70b45c899aca50d2448f2a1f15038c6bace6ef4b
OriginalKrepas <jjimmys1010@gmail.com>
2025-05-09 22:01:14 +0300

    Copy paste oti exeis sto branchsoy
    An den doylepsei kai twra spaw to laptop

--- app.py
+++ app.py
@@ -13,23 +13,12 @@ def init_db():
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
         c.execute('''
-                  
             CREATE TABLE users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT UNIQUE NOT NULL,
                 password TEXT NOT NULL,
                 admin INTEGER DEFAULT 0
             )
-            
-        ''')
-        c.execute('''
-                  
-            CREATE TABLE subjects (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                subject_name TEXT UNIQUE NOT NULL,
-                subject_semester TEXT
-            )
-                  
         ''')
         conn.commit()
         conn.close()
@@ -47,115 +36,6 @@ def create_superuser():
         print("✅ Superuser created.")
     else:
         print("ℹ️ Superuser already exists.")
-    conn.close()
-
-def create_subjects():
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS subjects (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            subject_name TEXT UNIQUE,
-            subject_semester TEXT
-        )
-    """)
-    c.executemany("""
-        INSERT OR IGNORE INTO subjects (subject_name, subject_semester)
-        VALUES (?, ?)
-    """, [
-        ('ΕΙΣΑΓΩΓΗ ΣΤΟΥΣ ΥΠΟΛΟΓΙΣΤΕΣ', '1'),
-        ('ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΜΟ ΥΠΟΛΟΓΙΣΤΩΝ', '1'),
-        ('ΜΑΘΗΜΑΤΙΚΗ ΑΝΑΛΥΣΗ 1', '1'),
-        ('ΗΛΕΚΤΡΟΜΑΓΝΗΤΙΣΜΟΣ-ΦΥΣΙΚΗ', '1'),
-        ('ΗΛΕΚΤΡΟΝΙΚΗ', '1'),
-        ('ΔΙΑΚΡΙΤΑ ΜΑΘΗΜΑΤΙΚΑ', '1'),
-
-        ('ΛΕΙΤΟΥΡΓΙΚΑ ΣΥΣΤΗΜΑΤΑ', '2'),
-        ('ΑΝΤΙΚΕΙΜΕΝΟΣΤΡΑΦΗΣ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟς ΥΠΟΛΟΓΙΣΤΩΝ C++', '2'),
-        ('ΜΑΘΗΜΑΜΤΙΚΗ ΑΝΑΛΥΣΗ 2', '2'),
-        ('ΓΡΑΜΜΙΚΗ ΑΛΓΕΒΡΑ', '2'),
-        ('ΣΥΝΔΥΑΣΤΙΚΑ ΨΗΦΙΑΚΑ ΗΛΕΚΤΡΟΝΙΚΑ', '2'),
-        ('ΑΓΓΛΙΚΑ ΟΡΟΛΟΓΙΑ ΠΛΗΡΟΦΟΡΙΚΗΣ 1', '2'),
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-        ('ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΣ ΣΤΟ ΔΙΑΔΥΚΤΥΟ', '3'),
-        ('ΜΕΤΑΓΛΩΤΤΙΣΤΕΣ', '3'),
-        ('ΑΡΙΘΜΗΤΙΚΗ ΑΝΑΛΥΣΗ', '3'),
-        ('ΠΙΘΑΝΟΤΗΤΕΣ-ΣΤΑΤΙΣΤΙΚΗ', '3'),
-        ('ΑΚΟΛΟΥΘΙΑΚΑ ΨΗΦΙΑΚΑ ΗΛΕΚΤΡΟΝΙΚΑ', '3'),
-        ('ΑΓΓΛΙΚΑ ΟΡΟΛΟΓΙΑ ΠΛΗΡΟΦΟΡΙΚΗΣ 2', '3'),
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-        ('ΔΙΚΤΥΑ ΥΠΟΛΟΓΙΣΤΩΝ', '4'),
-        ('ΒΑΣΕΙΣ ΔΕΔΟΜΕΝΩΝ', '4'),
-        ('ΜΙΚΡΟΕΠΕΞΕΡΓΑΣΤΕΣ-ΜΙΚΡΟΕΛΕΓΚΤΕΣ 1', '4'),
-        ('ΑΡΧΙΤΕΚΤΟΝΙΚΗ ΥΠΟΛΟΓΙΣΤΩΝ', '4'),
-        ('ΑΝΤΙΚΕΙΜΕΝΟΣΤΡΕΦΗΣ ΑΝΑΠΤΥΞΗ ΕΦΑΡΜΟΓΩΝ ΜΕ JAVA', '4'),
-        ('ΔΟΜΕΣ ΔΕΔΟΜΕΝΩΝ', '4'),
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-        ('ΚΑΤΑΝΕΜΗΜΕΝΑ ΣΥΣΤΗΜΑΤΑ', '5'),
-        ('ΑΛΓΟΡΙΘΜΟΙ ΚΑΙ ΠΟΛΥΠΛΟΚΟΤΗΤΑ', '5'),
-        ('ΤΕΧΝΟΛΟΓΙΑ ΛΟΓΙΣΜΙΚΟΥ', '5'),
-        ('ΣΧΕΔΙΑΣΗ ΔΙΚΤΥΩΝ ΥΠΟΛΟΓΙΣΤΩΝ', '5'),
-        ('ΕΙΔΙΚΑ ΘΕΜΑΤΑ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΥ', '5'),
-        ('ΕΦΑΡΜΟΣΜΕΝΑ ΜΑΘΗΜΑΤΙΚΑ', '5'),
-        ('ΤΕΧΝΟΛΟΓΙΑ ΠΟΛΥΜΕΣΩΝ', '5'),
-        ('ΑΛΛΗΛΕΠΙΔΡΑΣΗ ΑΝΘΡΩΠΟΥ-ΜΗΧΑΝΗΣ', '5'),
-        ('ΘΕΩΡΙΑ ΑΡΙΘΜΩΝ', '5'),
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-        ('ΑΣΦΑΛΕΙΑ ΥΠΟΛΟΓΙΣΤΗΚΩΝ ΣΥΣΤΗΜΑΤΩΝ', '6'),
-        ('ΤΕΧΝΗΤΗ ΝΟΗΜΟΣΥΝΗ', '6'),
-        ('ΤΗΛΕΠΙΚΟΙΝΩΝΙΕΣ', '6'),
-        ('ΣΧΕΔΙΑΣΗ ΨΗΦΙΑΚΩΝ ΣΥΣΤΗΜΑΤΩΝ ΜΕ VHDL', '6'),
-        ('ΑΣΥΡΜΑΤΕΣ ΚΙΝΗΤΕΣ ΕΠΙΚΟΙΝΩΝΙΕΣ', '6'),
-        ('ΓΡΑΦΙΚΑ ΜΕ ΥΠΟΛΟΓΙΣΤΕΣ', '6'),
-        ('ΠΡΟΧΩΡΗΜΕΝΑ ΘΕΜΑΤΑ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΥ ΙΣΤΟΥ 1', '6'),
-        ('ΕΔΙΚΑ ΘΕΜΑΤΑ ΔΙΚΤΥΩΝ 1', '6'),
-        ('ΥΠΟΛΟΓΙΣΤΙΚΑ ΝΕΦΗ', '6'),
-        ('ΘΕΜΑΤΑ ΑΡΙΘΜΗΤΙΚΗΣ ΑΝΑΛΥΣΗΣ', '6'),
-        ('ΑΡΙΘΜΗΤΙΚΗ ΕΠΙΛΥΣΗ ΔΙΑΦΟΡΚΩΝ ΕΞΙΣΩΣΕΩΝ', '6'),
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-        ('ΕΡΕΥΝΗΤΙΚΗ ΜΕΘΟΔΟΛΟΓΙΑ ΚΑΙ ΔΕΟΝΤΟΛΟΓΙΑ', '7'),
-        ('ΔΙΚΤΥΑ ΥΨΗΛΩΝ ΤΑΧΥΤΗΤΩΝ', '7'),
-        ('ΠΡΟΧΩΡΗΜΕΝΑ ΘΕΜΜΑΤΑ ΒΑΣΕΩΝ ΔΕΔΟΜΕΝΩΝ', '7'),
-        ('ΣΧΕΔΙΑΣΗ ΕΝΣΩΜΑΤΩΜΕΝΩΝ ΣΥΣΤΗΜΑΤΩΝ ΜΕ VLSI 1', '7'),
-        ('ΤΕΧΝΟΛΟΓΙΕΣ ΔΙΑΔΙΚΤΥΟΥ ΚΑΙ ΚΙΝΗΤΟΣ ΥΠΟΛΟΓΙΣΜΟΣ', '7'),
-        ('ΠΛΗΡΟΦΟΡΙΑΚΑ ΣΥΣΤΗΜΑΤΑ', '7'),
-        ('ΠΡΟΧΩΡΗΜΕΝΑ ΘΕΜΑΤΑ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΥ ΙΣΤΟΥ 2', '7'),
-        ('ΠΡΟΗΓΜΕΝΕΣ ΑΡΧΙΤΕΚΤΟΝΙΚΕΣ', '7'),
-        ('ΕΙΔΙΚΑ ΘΕΜΑΤΑ ΔΙΚΤΥΩΝ 2', '7'),
-        ('ΑΣΦΑΛΕΙΑ ΔΙΚΤΥΩΝ', '7'),
-        ('ΑΛΓΟΡΙΘΜΟΙ ΜΗΧΑΝΙΚΗΣ ΜΑΘΗΣΗΣ', '7'),
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-        ('ΨΗΦΙΑΚΗ ΕΠΕΞΕΡΓΑΣΙΑ ΣΗΜΑΤΟΣ', '8'),
-        ('ΟΠΤΙΚΟΣ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΣ', '8'),
-        ('ΜΙΚΡΟΕΠΕΞΕΡΓΑΣΤΕΣ-ΜΙΚΡΟΕΛΕΓΚΤΕΣ 2', '8'),
-        ('ΕΞΟΡΗΞΗ ΔΕΔΟΜΕΝΩΝ', '8'),
-        ('ΚΡΥΠΤΟΓΡΑΦΙΑ', '8'),
-        ('ΣΥΣΤΗΜΑΤΑ ΑΝΑΜΟΝΗΣ', '8'),
-        ('ΕΠΙΧΕΙΡΗΣΙΑΚΗ ΕΡΕΥΝΑ', '8'),
-        ('ΣΧΕΔΙΑΣΗ ΨΗΦΙΑΚΩΝ ΠΑΙΧΝΙΔΙΩΝ ΚΑΙ ΠΑΙΧΝΙΔΟΠΟΙΗΣΗ', '8'),
-        ('ΟΠΤΙΚΕΣ ΕΠΙΚΟΙΝΩΝΙΕΣ', '8'),
-        ('ΣΧΕΔΙΑΣΗ ΕΝΣΩΜΑΤΩΜΕΝΩΝ ΣΥΣΤΗΜΑΤΩΝ ΜΕ VLSI 2', '8'),
-        ('ΣΧΕΔΙΑΣΗ ΚΑΙ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΣ ΕΝΣΩΜΑΤΩΜΕΝΩΝ ΣΥΣΤΗΜΑΤΩΝ', '8')
-    ])
-    conn.commit()
     conn.close()
 
 @app.route('/')
@@ -201,21 +81,6 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/subjects')
-def subjects():
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    c.execute("SELECT subject_name, subject_semester FROM subjects")
-    rows = c.fetchall()
-    conn.close()
-
-    subjects_by_semester = {}
-    for name, semester in rows:
-        subjects_by_semester.setdefault(semester, []).append(name)
-
-    return render_template('subjects.html', subjects_by_semester=subjects_by_semester)
-
-
 @app.route('/logout')
 def logout():
     session.clear()
@@ -226,7 +91,6 @@ def dashboard():
     if 'username' not in session:
         return redirect(url_for('login'))
     return render_template('dashboard.html', username=session['username'], is_admin=session.get('admin', False))
-
 
 # Εκκίνηση εφαρμογής
 if __name__ == '__main__':
